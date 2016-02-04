edad2a8a61b87913cbd6ba4f8c2cdc7e98072c53
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-04 17:26:11 -0500

    adds verb fixtures

--- generator.js
+++ generator.js
@@ -1,21 +1,6 @@
 'use strict';
 
-module.exports = function (app) {
-  app.task('a', function(cb) {
-    console.log('foo > a');
-    cb();
-  });
-
-  app.task('b', function(cb) {
-    console.log('foo > b');
-    cb();
-  });
-
-  app.task('c', function(cb) {
-    console.log('foo > c');
-    cb();
-  });
-
+module.exports = function (app, base) {
   app.task('default', function(cb) {
     console.log('foo > default');
     cb();
---
+++ verbfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function (verb, base) {
+  verb.task('default', function(cb) {
+    console.log('generator-foo verbfile > default');
+    cb();
+  });
+
+  verb.task('a', function(cb) {
+    console.log('verb foo > a');
+    cb();
+  });
+
+  verb.task('b', function(cb) {
+    console.log('verb foo > b');
+    cb();
+  });
+
+  verb.task('c', function(cb) {
+    console.log('verb foo > c');
+    cb();
+  });
+};
